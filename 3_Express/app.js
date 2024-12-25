cfd0723061dbdd7df1518733c779bbef0ffa8a73
rushabh782 <rushabhpatel877@gmail.com>
2024-12-25 14:06:30 +0530

    worked with GET,POST,PUT,DELETE request

--- 3_Express/app.js
+++ 3_Express/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 //get , post, put, delete
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "Javascript" },
   { id: 2, name: "Java" },
@@ -20,6 +22,50 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.send("Contact Us at abcd@abcd.com");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+}); //read
+
+app.post("/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+}); //create
+
+//Put method - update already existing entries
+app.put("/courses/:coursename", (req, res) => {
+  let course = courses.find((course) => course.name === req.params.coursename);
+  if (!course)
+    res.status(404).send("The course you are looking for does not exist");
+
+  course.name = req.params.name;
+  res.send(course);
+}); //update
+
+// app.delete("/courses/:coursename", (req, res) => {
+//   let UpdatedCourses = courses.filter(
+//     (course) => course.name !== req.params.coursename
+//   );
+
+//   courses = UpdatedCourses;
+
+//   res.send(courses);
+// });
+
+app.delete("/courses/:id", (req, res) => {
+  let course = courses.find((course) => course.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course you are looking for does not exist");
+
+  const index = courses.indexOf(course);
+
+  courses.splice(index, 1);
+  res.send(course);
 });
 
 //Route Parameters
