f3b208c22dfd295edd3acce907ceaad2ef374f2a
rushabh782 <rushabhpatel877@gmail.com>
2024-12-27 13:57:21 +0530

    added files for Asynchronous JS

---
+++ Module 7 (Asynchronous Js)/cb.js
@@ -0,0 +1,33 @@
+const fs = require("fs"); //importing file system module
+
+console.log("First Line");
+
+// let data = fs.readFileSync("f1.txt");
+
+// console.log("File 1 Data ->" + data);
+
+// let data2 = fs.readFileSync("f2.txt");
+
+// console.log("File 2 Data ->" + data2);
+
+fs.readFile("f1.txt", cb1);
+
+function cb1(err, data) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File 1 Data -> " + data);
+}
+
+fs.readFile("f2.txt", cb2);
+
+function cb2(err, data) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File 2 Data -> " + data);
+}
+
+console.log("Last Line");
